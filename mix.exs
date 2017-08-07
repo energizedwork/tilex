d980501f4e687f0c0d7ac3fca2ccd8087b6e4f56
msp <matt@cenatus.org>
2017-08-07 18:23:25 +0100

    Don't create the DB for Heroku CI

--- mix.exs
+++ mix.exs
@@ -77,6 +77,6 @@ defmodule Tilex.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.migrate", "test"]]
   end
 end
