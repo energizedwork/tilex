341c59532c5a3e937cb6856e577f36ad9e9e63e4
msp <matt@cenatus.org>
2017-08-07 17:34:31 +0100

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tilex, TilexWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "til.hashrocket.com", port: 80],
+  url: [scheme: "https", host: "ew-til-staging.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
